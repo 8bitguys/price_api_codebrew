19c00a070c43197710b75eb7dc3161356dff888a
combosm <jakeshellypaul@gmail.com>
2025-04-26 11:43:31 +1000

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from simple_predict import predict_price
 
@@ -6,14 +7,14 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-
     if not data or 'features' not in data:
         return jsonify({'error': 'Missing features field'}), 400
 
-    features_string = data['features']  # Should be a comma-separated string
+    features_string = data['features']
     prediction = predict_price(features_string)
 
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # <-- This is critical
+    app.run(host='0.0.0.0', port=port, debug=True)
